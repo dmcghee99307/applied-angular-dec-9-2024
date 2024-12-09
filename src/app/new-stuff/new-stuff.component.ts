6ba66d8cefc4fee643eecd57b3c7d00e9852f039
Jeffry Gonzalez <jeff@hypertheory.com>
2024-12-09 16:29:11 -0500

    done with monday

--- src/app/new-stuff/new-stuff.component.ts
+++ src/app/new-stuff/new-stuff.component.ts
@@ -1,65 +1,33 @@
-import {
-  Component,
-  ChangeDetectionStrategy,
-  signal,
-  computed,
-} from '@angular/core';
-import { FriendListComponent } from './components/friend-list.component';
-import { Friend } from './types';
-import { FriendStatsComponent } from './components/friend-stats.component';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FriendCreateComponent } from './components/friend-create.component';
+import { FriendListComponent } from './components/friend-list.component';
+import { FriendStatsComponent } from './components/friend-stats.component';
+import { FriendsStore } from './services/friends.store';
 
 @Component({
   selector: 'app-new-stuff',
+
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [FriendListComponent, FriendStatsComponent, FriendCreateComponent],
   template: `
     <p>Going out to meals with friends is fun.</p>
-    <app-friend-stats [numberOfFriends]="numberOfFriends()" />
+    <app-friend-stats [numberOfFriends]="store.numberOfFriends()" />
     <app-friend-list
-      [peopleToList]="friends()"
-      (personSelected)="selectedFriend.set($event)"
+      [peopleToList]="store.entities()"
+      (personSelected)="store.setSelectedFriend($event)"
     />
 
-    @if (selectedFriend() !== undefined) {
-      <p>You selected {{ selectedFriend()?.name }}</p>
-      <button class="btn btn-danger" (click)="unfriend()">
+    @if (store.selectedFriend() !== undefined) {
+      <p>You selected {{ store.selectedFriend()?.name }}</p>
+      <button class="btn btn-danger" (click)="store.unFriend()">
         Unfriend This Person?
       </button>
     }
 
-    <app-friend-create (personAdded)="addFriend($event)" />
+    <app-friend-create (personAdded)="store.addFriend($event)" />
   `,
   styles: ``,
 })
 export class NewStuffComponent {
-  selectedFriend = signal<Friend | undefined>(undefined);
-
-  numberOfFriends = computed(() => this.friends().length);
-  friends = signal<Friend[]>([
-    {
-      id: '1',
-      name: 'Jill Jones',
-    },
-    {
-      id: '2',
-      name: 'Steve Smith',
-    },
-  ]);
-
-  addFriend(name: string) {
-    const friendToAdd: Friend = {
-      name,
-      id: crypto.randomUUID(),
-    };
-    this.friends.set([friendToAdd, ...this.friends()]);
-  }
-  unfriend() {
-    if (this.selectedFriend()) {
-      this.friends.update((f) =>
-        f.filter((f) => f.id !== this.selectedFriend()?.id),
-      );
-      this.selectedFriend.set(undefined);
-    }
-  }
+  store = inject(FriendsStore);
 }
