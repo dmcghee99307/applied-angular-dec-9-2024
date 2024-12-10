a024d1639b726acdd76afef86291bb1c7e2190f8
Jeffry Gonzalez <jeff@hypertheory.com>
2024-12-10 11:40:32 -0500

    refactor begun

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,10 +9,10 @@ export const routes: Routes = [
     path: 'welcome',
     component: WelcomeComponent,
   },
-
   {
     path: 'meals',
     loadChildren: () =>
+      // we are doing this not to lazy load, but to put it in a separate bundle.
       import('./meals/meals.routes').then((c) => c.MEAL_ROUTES),
   },
   {
--- src/app/meals/meals.component.ts
+++ src/app/meals/meals.component.ts
@@ -1,18 +1,39 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FriendStatsComponent } from './components/friend-stats.component';
 import { FriendsStore } from './services/friends.store';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-meals',
 
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [FriendStatsComponent, RouterOutlet],
+  imports: [RouterOutlet, RouterLink, FriendStatsComponent],
   template: `
-    <p>Meals With Friends!</p>
-    <app-friend-stats [numberOfFriends]="store.numberOfFriends()" />
-
-    <router-outlet />
+    <div class="drawer lg:drawer-open">
+      <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+      <div class="drawer-content flex flex-col items-center justify-center">
+        <app-friend-stats [numberOfFriends]="store.numberOfFriends()" />
+        <router-outlet />
+        <label
+          for="my-drawer-2"
+          class="btn btn-primary drawer-button lg:hidden"
+        >
+          Open drawer
+        </label>
+      </div>
+      <div class="drawer-side">
+        <label
+          for="my-drawer-2"
+          aria-label="close sidebar"
+          class="drawer-overlay"
+        ></label>
+        <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+          <!-- Sidebar content here -->
+          <li><a routerLink="list">List of Friends</a></li>
+          <li><a routerLink="create">Add A Friend</a></li>
+        </ul>
+      </div>
+    </div>
   `,
   styles: ``,
 })
